5366bd2a8d8f9608759a26087787c8a81df12316
Ouedraogo-junior <koumboujunior.com@gmail.com>
2026-04-29 12:57:43 +0000

    Ajout de pwa

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,29 @@
-/** @type {import('next').NextConfig} */
+import withPWAInit from '@ducanh2912/next-pwa'
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/wpcqvhtmlibklpgrprig\.supabase\.co\/.*/i,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'supabase-images',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24 * 7,
+        },
+      },
+    },
+  ],
+})
+
 const nextConfig = {
+  reactStrictMode: true,
+  turbopack: {},
+  allowedDevOrigins: ['192.168.1.81'],
   images: {
     remotePatterns: [
       {
@@ -10,4 +34,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+export default withPWA(nextConfig)
